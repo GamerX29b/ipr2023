99a6a1ea1136d379f54632351d4d05afcc6d724d
serge <sergej-93@mail.ru>
2023-07-08 14:03:07 +0600

    Собирается, деплоится, но не работает.

--- alphaProject/src/main/java/webService/WebTemperature.java
+++ alphaProject/src/main/java/webService/WebTemperature.java
@@ -4,18 +4,25 @@ import dto.Weather;
 import facade.WeatherFacade;
 
 import javax.annotation.Resource;
-import javax.ejb.Stateless;
-import javax.jws.WebService;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@Stateless
-@WebService(endpointInterface = "webService.WebTemperatureService")
-public class WebTemperature implements WebTemperatureService{
+@WebServlet("/temperature")
+public class WebTemperature extends HttpServlet implements WebTemperatureService{
 
     @Resource(name = "jdbc/WeatherFacade")
     WeatherFacade weatherFacade;
 
-    @Override
-    public Weather takeTemperature(){
-        return weatherFacade.temperature();
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>" + weatherFacade.temperature() + "</h1>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
--- alphaProject/src/main/java/webService/WebTemperatureService.java
+++ alphaProject/src/main/java/webService/WebTemperatureService.java
@@ -4,10 +4,13 @@ import dto.Weather;
 
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebService
 @SOAPBinding(style=SOAPBinding.Style.RPC,use=SOAPBinding.Use.ENCODED)
 public interface WebTemperatureService {
 
-    Weather takeTemperature();
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException;
 }
